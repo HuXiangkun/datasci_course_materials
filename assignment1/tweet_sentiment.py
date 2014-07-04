67806c798591400e081a938291a9406757c63f5f
HuXiangkun <huxk_hit@qq.com>
2014-07-04 17:23:37 +0800

    增加从twitter上下载的数据，p1和p2的实现

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,17 +1,37 @@
 import sys
+import json
 
-def hw():
-    print 'Hello, world!'
+scores = {}
+tweets = []
 
-def lines(fp):
-    print str(len(fp.readlines()))
+def loadAFINN(sent_file):
+    for line in sent_file:
+        term, score = line.split("\t")
+        scores[term] = int(score)
+
+
+def loadTwitts(tweet_file):
+    for line in tweet_file.readlines():
+        t = json.loads(line)
+        if t.has_key("created_at"):
+            tweets.append(t['text'].encode('utf-8'))
+
+def printSentiment():
+    for tweet in tweets:
+        score = 0
+        words = []
+        words = tweet.split(" ")
+        for word in words:
+            if word in scores.keys():
+                score += scores[word]
+        print score
 
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    loadAFINN(sent_file)
+    loadTwitts(tweet_file)
+    printSentiment()
 
 if __name__ == '__main__':
     main()
